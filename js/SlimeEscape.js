a01912456e68d1deae24ea99bec8c7f4308c7f69
jvkoh <jonathanvkoh@gmail.com>
2015-08-22 12:47:21 -0400

    Adding basic Phaser to site

---
+++ js/SlimeEscape.js
@@ -0,0 +1,20 @@
+define([
+    'Base'
+], function(
+    Base
+) {
+
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+    function preload() {
+    }
+
+
+    function create() {
+    }
+
+
+    function update() {
+    }
+
+});
