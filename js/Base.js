cde1495ddaa637a6fc4e24f2c4e5fa8ab37b66c3
jvkoh <jonathanvkoh@gmail.com>
2015-08-22 13:02:07 -0400

    Add javascript base class

--- js/Base.js
+++ js/Base.js
@@ -1,5 +1,29 @@
 define([], function() {
 
-    console.log("this runs!");
+    var Base = function Base() {};
+
+    Base.extend = function extend() {
+        for (var i = 0; i < arguments.length; i++) {
+            var obj = arguments[i];
+
+            if (typeof obj !== 'object') {
+                continue;
+            }
+
+            if (Array.isArray(obj)) {
+                this.extend.apply(this, obj);
+            } else {
+                for (var property in obj) {
+                    if (property) {
+                        this.prototype[property] = obj[property];
+                    }
+                }
+            }
+        }
+
+        return this;
+    };
+
+    return Base;
 
 });
